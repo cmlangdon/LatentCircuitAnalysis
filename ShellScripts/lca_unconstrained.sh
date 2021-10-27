8d32ba4b8b8aa65541181dc93ffebcff0b38350e
cmlangdon <langdon@cshl.edu>
2021-10-27 13:59:32 -0400

    Deleted shell script folder

--- ShellScripts/lca_unconstrained.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-#$ -t 1-30
-#$ -N lca
-#$-cwd
-#$ -l gpu=1
-#$ -l m_mem_free=20G
-python -u lca_unconstrained.py
--- lca_unconstrained.sh
+++ lca_unconstrained.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
-#$ -t 1-125
+#$ -t 1-30
 #$ -N lca
 #$-cwd
+#$ -l gpu=1
 #$ -l m_mem_free=20G
 python -u lca_unconstrained.py
